e4d084b89729b7ba8897bca8f4d483bacb89f043
keybig <100385397+keybig@users.noreply.github.com>
2023-11-22 21:11:51 -0500

    include all addresses owned by account

--- ui/src/hooks/useWalletAddress.ts
+++ ui/src/hooks/useWalletAddress.ts
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useKwentaAccount } from './useKwentaAccount';
 import { usePolynomialAccount } from './usePolynomialAccount';
@@ -6,41 +5,28 @@ import { useOwnerKwenta, useOwnerPolynomial } from './useOwnerBySmartId';
 
 export const useWalletAddress = () => {
   const { walletAddress } = useParams();
-  const [address, setAddress] = useState<string | null>(null);
-  const [allAddresses, setallAddresses] = useState<string[]>([]);
 
   const { data: kwentaAccount } = useKwentaAccount(walletAddress?.toLowerCase());
   const { kwentaOwner } = useOwnerKwenta(walletAddress?.toLowerCase());
   const { data: polynomialAccount } = usePolynomialAccount(walletAddress?.toLowerCase());
   const { polynomialOwner } = useOwnerPolynomial(walletAddress?.toLowerCase());
 
-  useEffect(() => {
-    if (walletAddress && walletAddress.toLowerCase() !== address) {
-      setAddress(walletAddress.toLowerCase());
+  // get all possible addresses belonging to a single account
+  const potentialAddresses = [
+    walletAddress?.toLowerCase(),
+    kwentaAccount?.account, //
+    kwentaAccount?.owner,
+    kwentaOwner,
+    polynomialAccount?.account,
+    polynomialAccount?.owner,
+    polynomialOwner,
+  ].filter((address): address is string => Boolean(address));
 
-      const potentialAddresses = [
-        walletAddress?.toLowerCase(),
-        kwentaAccount?.account,
-        kwentaAccount?.owner,
-        kwentaOwner,
-        polynomialAccount?.account,
-        polynomialAccount?.owner,
-        polynomialOwner,
-      ].filter((address): address is string => Boolean(address));
-
-      const uniqueAddresses = Array.from(new Set(potentialAddresses));
-
-      const newArr = JSON.stringify(uniqueAddresses.sort());
-      const stateArr = JSON.stringify(allAddresses.sort());
-
-      if (newArr !== stateArr) {
-        setallAddresses(uniqueAddresses);
-      }
-    }
-  }, [walletAddress]);
+  // remove any duplicate addresses
+  const uniqueAddresses = Array.from(new Set(potentialAddresses));
+  const allAddresses = uniqueAddresses.sort();
 
   return {
-    address,
     allAddresses,
   };
 };
