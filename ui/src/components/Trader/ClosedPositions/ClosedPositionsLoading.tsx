216d165956d04e36ed072c3c5cf73e3b0f966347
keybig <100385397+keybig@users.noreply.github.com>
2023-11-29 12:20:24 -0500

    move skeleton circle icon to start

--- ui/src/components/Trader/ClosedPositions/ClosedPositionsLoading.tsx
+++ ui/src/components/Trader/ClosedPositions/ClosedPositionsLoading.tsx
@@ -3,18 +3,17 @@ import { SkeletonCircle, Skeleton, Td, Tr, Box, Flex } from '@chakra-ui/react';
 export const ClosedPositionsLoading = () => {
   const createSkeletonRow = () => (
     <Tr borderTopWidth="1px">
-      <Box as={Td} border="none">
-        <Skeleton startColor="gray.700" endColor="gray.900" my={2}>
-          Lorem Ipsum
-        </Skeleton>
-      </Box>
-
       <Flex as={Td} border="none" alignItems="center">
         <SkeletonCircle mr={2} startColor="gray.700" endColor="gray.900" />
         <Skeleton startColor="gray.700" endColor="gray.900" my={2}>
           Lorem Ipsum
         </Skeleton>
       </Flex>
+      <Box as={Td} border="none">
+        <Skeleton startColor="gray.700" endColor="gray.900" my={2}>
+          Lorem Ipsum
+        </Skeleton>
+      </Box>
 
       <Box as={Td} border="none">
         <Skeleton startColor="gray.700" endColor="gray.900" my={2}>
